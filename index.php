2bcb92b9e99cfec31a4460e6694008f057cbbb3f
Tommy Henry <then815@yahoo.com>
2017-06-12 09:14:05 -0500

    Added Projects

--- index.php
+++ index.php
@@ -3,4 +3,98 @@
     include("includes/incHeader.php");
 ?>
 <body>
+    <header id="top">
+        <h1><span class="logoLetters">EH</span>Edwards<br/>Huffington</h1>
+        <nav>
+            <ul>
+                <li><a href="#top">Home</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#products">Products</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+        <button><a href=# title="Get Quote">Get Quote</a></button>
+    </header>
+    <div class="hero">
+            <img src="imgs/heroHeader.png" alt="Worker Servicing Switchboard"/>
+    </div>
+    <main id="services">
+        <h2>Services</h2>
+        <p>Gag ipsum dolar sit amet homework dog ba dumm tss bart happy russia lois. Cool anonymous bottom megusta genius asian geek sister coffee. German hac silent hill 140% troll not bad loki easter students.</p>
+        <ul>
+            <li>
+                <img src="imgs/cameraIcon.png" alt="Video camera icon"/><br/>
+                <h3>Security</h3>
+                <p>Just yao drink nother to do here nap party monocle homer. Phone in class keyboard avenger computer dead simpson one does not simply house laptop.</p>
+            </li>
+            <li>
+                <img src="imgs/paperIcon.png" alt="Paper icon"/><br/>
+                <h3>Data</h3>
+                <p>Just yao drink nother to do here nap party monocle homer. Phone in class keyboard avenger computer dead simpson one does not simply house laptop.</p>
+            </li>
+            <li>
+                <img src="imgs/toolIcon.png" alt="Tools icon"/><br/>
+                <h3>Tools</h3>
+                <p>Just yao drink nother to do here nap party monocle homer. Phone in class keyboard avenger computer dead simpson one does not simply house laptop.</p>
+            </li>
+            <li>
+                <img src="imgs/pluginIcon.png" alt="Plugin icon"/><br/>
+                <h3>Plug-Ins</h3>
+                <p>Just yao drink nother to do here nap party monocle homer. Phone in class keyboard avenger computer dead simpson one does not simply house laptop.</p>
+            </li>
+        </ul>
+        <div class="workersBox">
+            <h2>We Work Smarter</h2>
+            <img src="imgs/workerLeft.png" class="leftWorker" alt="yellow hat working on switchboard"/>
+            <img src="imgs/workerRight.png" class="rightWorker" alt="blue hat working on switchboard"/>
+            <h3>Alcohol bear close enough derpina top good guy bra read captain. Cellphone high school portfolio fun diablo 3 mother thor clinton elephant. </h3>
+            <p>Puking Rainbows for if you know what i mean scumbag forever alone gag challenge accepted sandwich. Architect le girlfriend really? 9000 joke like a boss charlie sheen luke dummies rebecca black crying trolololo. All the Things avengers i know that feel female cat essay oh stop it, you twitter happy wodka the avengers. On unsave steve jobs problem? In aww yeah men lol pizza dolar face.</p>
+            <button><a href=# title="Get Quote">Get Quote</a></button>
+        </div>
+        <div class="workflowBox">
+            <h2>Our Workflow</h2>
+            <p>Hammer feel like a sir bart why t-rex cereal in fun essay. Too mainstream i see what you did there varius homer le friend top brother oh stop it, you. Loki you don't say?</p>
+            <ul>
+                <li>
+                    <h3>Consultation</h3>
+                    <p>Okay nyan cat house unlock party trolololo lol all the things meme. Happy derp troll like a boss avenger people sandwich always read genius.</p>
+                </li>
+                <li>
+                    <h3>Construction</h3>
+                    <p>Iron man problem? Good guy le derp superhero drink note close enough just amnesia simpson pizza one does not simply.</p>
+                </li>
+                <li>
+                    <h3>Certification</h3>
+                    <p>Tank cellphone anonymous russia geek in class impossibru grin avengers. Genius 9000 architect monday facebook alcohol panda forever alone. </p>
+                </li>
+            </ul>
+        </div>
+    </main>
+    <div class="banner" id="products">
+        <h2>Product Search</h2>
+        <form>
+            <input value="Product Name"/>
+            <input value="Product Number"/>
+        </form>
+        <button>Search</button>
+    </div>
+    <div id="projects">
+        <h2>Projects</h2>
+        <ul class="first">
+            <li><a href="#"><img src="imgs/handsWorking.png" alt="hands working"><br/><span class="caption">Lorem Ipsum Dolor</span></a></li>
+            <li><a href="#"><img src="imgs/breakerImage.png" alt="breaker"><br/><span class="caption">Lorem Ipsum Dolor</span></a></li>
+            <li><a href="#"><img src="imgs/railingImage.png" alt="inside railing"><br/><span class="caption">Lorem Ipsum Dolor</span></a></li>
+            <li><a href="#"><img src="imgs/drillWiring.png" alt="drill wiring"><br/><span class="caption">Lorem Ipsum Dolor</span></a></li>
+        </ul>
+        <ul class="second">
+            <li><a href="#"><img src="imgs/laddersOutside.png" alt="ladders outside"><br/><span class="caption">Lorem Ipsum Dolor</span></a></li>
+            <li><a href="#"><img src="imgs/hallwayImage.png" alt="hallway"><br/><span class="caption">Lorem Ipsum Dolor</span></a></li>
+            <li><a href="#"><img src="imgs/insideLoft.png" alt="inside loft"><br/><span class="caption">Lorem Ipsum Dolor</span></a></li>
+            <li><a href="#"><img src="imgs/insideWiring.png" alt="inside wiring"><br/><span class="caption">Lorem Ipsum Dolor</span></a></li>
+        </ul>
+    </div>
+    
 </body>
+</html>
